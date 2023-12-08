890c21baa21460ef158835ae95795ac9b80fcdbc
ujicama <ujicama@DESKTOP-FU7URBK.localdomain>
2023-12-08 20:24:44 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from transformers import pipeline
+
+def analyze_sentiment(text):
+    classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+    result = classifier(text)
+    return {"label": result[0]["label"], "score": result[0]["score"]}
+
+def main():
+    st.title("Sentiment tonality analyzer")
+
+    user_input = st.text_area("Enter text for tonality analysis:", "")
+
+    if st.button("Analyze"):
+        if user_input:
+            result = analyze_sentiment(user_input)
+            st.subheader("Analysis result:")
+            st.write(f"Tonality: {result['label']}")
+            st.write(f"Confidence: {result['score']:.4f}")
+        else:
+            st.warning("Please enter text for analysis.")
+
+if __name__ == "__main__":
+    main()
